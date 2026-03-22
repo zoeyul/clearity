63f824653273bb3ab988e240f6970a9bd5d25894
Seoyul Kim <zoeyul07@gmail.com>
2026-03-22 21:26:09 +0900

    set desktop version

--- apps/mobile/capacitor.config.ts
+++ apps/mobile/capacitor.config.ts
@@ -3,8 +3,10 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "com.clearity.app",
   appName: "Clearity",
-  webDir: "../web/out",
+  webDir: "www",
   server: {
+    url: "https://clearity-taupe.vercel.app",
+    cleartext: false,
     androidScheme: "https",
   },
 };
